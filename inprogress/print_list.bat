86853206c07e4f6dfed2ab9317a1a9cace5da7e7
cainchen <shu90129@gmail.com>
2018-01-28 00:34:09 +0800

    Initialization commit to branch 'inprogress' Jan 28,2018

---
+++ inprogress/print_list.bat
@@ -0,0 +1,13 @@
+; This BAT is used for print data on monitor from a specific file, and only print to a specified start line to the end line.
+@echo off & setlocal ENABLEEXTENSIONS
+set start=5
+set "lines=8"
+set/a i=-1,start-=1
+set "ok=" 
+for /f "delims=" %%a in ('more/e +%start% ^< d:\lab\1.txt') do (
+set/a i+=1 & for /f %%z in ('echo/%%i%%') do (
+if "%%z"=="%lines%" set ok=1
+)
+if not defined ok echo/%%a
+)
+pause>nul
